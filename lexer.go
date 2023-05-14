67ab9b2779e7e839bc37a36e0ea8e1b1711d9a5e
GiuseppeMuntoni <g.muntoni@consorziometis.it>
2023-05-14 12:11:05 +0200

    match comments and single-char tokens

--- lexer.go
+++ lexer.go
@@ -2,28 +2,47 @@ package dotparser
 
 import (
 	"bufio"
+	"errors"
 	"io"
+	"unicode"
 )
 
 type Token int
+type Lexeme string
 
 const (
-	GRAPH Token = iota
-	DIGRAPH
-	STRICT
-	OPEN_BRACE
+	// Single-char tokens
+	OPEN_BRACE Token = iota
 	CLOSE_BRACE
 	SEMICOLON
 	COLON
 	COMMA
-	NODE
-	EDGE
 	OPEN_SQUARE_BRACKET
 	CLOSE_SQUARE_BRACKET
 	EQUAL
+
+	// Literals
+	ID
+
+	// Keywords
+	GRAPH
+	DIGRAPH
+	STRICT
+	NODE
+	EDGE
 	SUBGRAPH
+
 	EOF
 )
+
+var keywords = map[string]Token{
+	"graph":    GRAPH,
+	"digraph":  DIGRAPH,
+	"strict":   STRICT,
+	"node":     NODE,
+	"edge":     EDGE,
+	"subgraph": SUBGRAPH,
+}
 
 type Position struct {
 	line   int
@@ -37,30 +56,138 @@ type Lexer struct {
 
 func New(reader io.Reader) *Lexer {
 	return &Lexer{
-		position: Position{line: 1, column: 0},
+		position: Position{line: 1, column: 1},
 		reader:   bufio.NewReader(reader),
 	}
 }
 
-func (l *Lexer) Lex() (Position, Token, string) {
+func (lexer *Lexer) Lex() (Position, Token, Lexeme) {
 	// keep looping until we return a token
 	for {
-		r, _, err := l.reader.ReadRune()
+		char, err := lexer.advance()
+
 		if err != nil {
 			if err == io.EOF {
-				return l.pos, EOF, ""
+				return lexer.position, EOF, ""
 			}
-
 			panic(err)
+		}
+
+		if unicode.IsSpace(char) {
+			continue
+		}
+
+		switch char {
+		// match single-char tokens
+		case '{':
+			lexer.addToken(OPEN_BRACE)
+		case '}':
+			lexer.addToken(CLOSE_BRACE)
+		case ';':
+			lexer.addToken(SEMICOLON)
+		case ':':
+			lexer.addToken(COLON)
+		case ',':
+			lexer.addToken(COMMA)
+		case '[':
+			lexer.addToken(OPEN_SQUARE_BRACKET)
+		case ']':
+			lexer.addToken(CLOSE_SQUARE_BRACKET)
+		case '=':
+			lexer.addToken(EQUAL)
+		// match comments
+		case '#':
+			fallthrough
+		case '/':
+			lexer.matchComment(char)
 		}
 	}
 }
 
-func (lexer *Lexer) advance() {
-	lexer.position.column += 1
+func (lexer *Lexer) matchComment(firstChar rune) error {
+	if firstChar == '#' {
+		if lexer.position.column != 1 {
+			return errors.New("")
+		} else {
+			lexer.skipLine()
+			return nil
+		}
+	} else if firstChar == '/' {
+		char, err := lexer.advance()
+		if err != nil {
+			return errors.New("")
+		}
+		if char == '/' {
+			lexer.skipLine()
+			return nil
+		} else if char == '*' {
+			lexer.skipMultiLineComment()
+			return nil
+		} else {
+			return errors.New("")
+		}
+	} else {
+		return errors.New("")
+	}
 }
 
-func (l *Lexer) newLine() {
-	l.pos.line++
-	l.pos.column = 1
+func matchKeyword(ide string) (Token, Lexeme) {
+	token, exist := keywords[ide]
+	if exist {
+		return token, ""
+	} else {
+		return ID, Lexeme(ide)
+	}
+}
+
+func (lexer *Lexer) addToken(token Token) (Position, Token, Lexeme) {
+	return lexer.position, token, ""
+}
+
+func (lexer *Lexer) advance() (char rune, err error) {
+	char, _, err = lexer.reader.ReadRune()
+	lexer.position.column += 1
+	return
+}
+
+func (lexer *Lexer) skipLine() {
+	for {
+		char, err := lexer.advance()
+		if err != nil {
+			lexer.reader.UnreadRune()
+			return
+		}
+		if char == '\n' {
+			lexer.newLine()
+			return
+		}
+	}
+}
+
+func (lexer *Lexer) skipMultiLineComment() {
+	for {
+		char, err := lexer.advance()
+		if err != nil {
+			lexer.reader.UnreadRune()
+			return
+		}
+		if char == '\n' {
+			lexer.newLine()
+		} else if char == '*' {
+			lexer.advance()
+			char, err := lexer.advance()
+			if err != nil {
+				lexer.reader.UnreadRune()
+				return
+			}
+			if char == '/' {
+				return
+			}
+		}
+	}
+}
+
+func (lexer *Lexer) newLine() {
+	lexer.position.line++
+	lexer.position.column = 1
 }
